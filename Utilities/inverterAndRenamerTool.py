b7c59c2cb80c557ea3a5e416d92709bf6b185b65
Jonathan Matthews <jono@midway2-login1.rcc.local>
2021-06-07 10:48:43 -0500

    .

--- Utilities/inverterAndRenamerTool.py
+++ Utilities/inverterAndRenamerTool.py
@@ -6,10 +6,11 @@ path = Path(r"/home/jono/ML/RawData/Jono_2021Masks/202105_OriginalY")
 fileList = [imagePath for imagePath in sorted(path.iterdir()) if imagePath.is_file()]
 
 for imageFilename in fileList:
-    imageFile = Image.open(imageFilename).convert(mode="1")
-    inverted = ImageOps.invert(imageFile)
+    print("Opening " + str(imageFilename))
+    imageFile = Image.open(imageFilename).convert(mode="L")
+    inverted = ImageOps.invert(imageFile).convert(mode="1")
 
     formattedFilename = imageFilename.absolute().parent / imageFilename.name[16:]
-
+    print("Saving to " + str(formattedFilename))
     inverted.save(formattedFilename)
 
