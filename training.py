96bdf7199532381806491d17dfaa5d1010bfae2d
Jono Matthews <jono.jmm@gmail.com>
2021-06-15 15:07:00 -0500

    .

--- training.py
+++ training.py
@@ -13,18 +13,8 @@ from sklearn.model_selection import train_test_split
 from pathlib import Path
 import typing
 
-import traceback
-
 import dill
 from realTimeData import RealTimeData
-
-import os
-
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-
-
-# for gpu in tf.config.list_physical_devices('GPU'):
-#     tf.config.experimental.set_memory_growth(gpu, True)
 
 
 class RealTimeCallback(Callback):
@@ -144,51 +134,51 @@ def FitModel(trainingImagesPath: Path, trainingSegmentationsPath: Path, outputPa
     inputs = Input((imageSize[0], imageSize[1], 1))
 
     c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(inputs)
-    # c1 = Dropout(dropout_rate)(c1)
+    c1 = Dropout(dropout_rate)(c1)
     c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c1)
 
     p1 = MaxPooling2D((2, 2))(c1)
     c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p1)
-    # c2 = Dropout(dropout_rate)(c2)
+    c2 = Dropout(dropout_rate)(c2)
     c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c2)
 
     p2 = MaxPooling2D((2, 2))(c2)
     c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p2)
-    # c3 = Dropout(dropout_rate)(c3)
+    c3 = Dropout(dropout_rate)(c3)
     c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c3)
 
     p3 = MaxPooling2D((2, 2))(c3)
     c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p3)
-    # c4 = Dropout(dropout_rate)(c4)
+    c4 = Dropout(dropout_rate)(c4)
     c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c4)
 
     p4 = MaxPooling2D(pool_size=(2, 2))(c4)
     c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(p4)
-    # c5 = Dropout(dropout_rate)(c5)
+    c5 = Dropout(dropout_rate)(c5)
     c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c5)
 
     u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
     u6 = concatenate([u6, c4])
     c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u6)
-    # c6 = Dropout(dropout_rate)(c6)
+    c6 = Dropout(dropout_rate)(c6)
     c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c6)
 
     u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
     u7 = concatenate([u7, c3])
     c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u7)
-    # c7 = Dropout(dropout_rate)(c7)
+    c7 = Dropout(dropout_rate)(c7)
     c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c7)
 
     u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
     u8 = concatenate([u8, c2])
     c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u8)
-    # c8 = Dropout(dropout_rate)(c8)
+    c8 = Dropout(dropout_rate)(c8)
     c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c8)
 
     u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
     u9 = concatenate([u9, c1])
     c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(u9)
-    # c9 = Dropout(dropout_rate)(c9)
+    c9 = Dropout(dropout_rate)(c9)
     c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c9)
 
     final = Conv2D(1, (1, 1), activation='sigmoid')(c9)
@@ -206,11 +196,11 @@ def FitModel(trainingImagesPath: Path, trainingSegmentationsPath: Path, outputPa
     outputPath.mkdir(parents=True, exist_ok=True)
     print("\tFitting model...", flush=True)
     model.fit(ImagesLoader(trainingImagePaths, trainingSegmentationPaths, imageSize, batch_size),
-              # validation_data=ImagesLoader(testingImagePaths, testingSegmentationPaths, imageSize, batch_size),
+              validation_data=ImagesLoader(testingImagePaths, testingSegmentationPaths, imageSize, batch_size),
               verbose=1,
               epochs=epochs,
               steps_per_epoch=batches,
-              # validation_steps=int(len(testingImagePaths) / batch_size),
+              validation_steps=int(len(testingImagePaths) / batch_size),
               callbacks=[earlystopper])
     print("\tDone!", flush=True)
 
