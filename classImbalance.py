ca10dcf8cc7546eb89ff78e99ec44ed12a0a378e
Jono Matthews <jono.jmm@gmail.com>
2021-06-22 16:59:02 -0500

    .

--- classImbalance.py
+++ classImbalance.py
@@ -7,15 +7,17 @@ def ComputeClassImbalance(segmentationsDirectory: Path):
     files = segmentationsDirectory.iterdir()
     totalZeros = 0
     totalOnes = 0
-    for file in files:
-        segmentation = Image.open(file)
+    for fileIndex, fileName in enumerate(files):
+        segmentation = Image.open(fileName)
         image = np.array(segmentation.convert(mode="1"))
         size = np.size(image)
         ones = np.count_nonzero(image)
         zeros = size - ones
         totalZeros += zeros
         totalOnes += ones
-    print("Zeros: " + str(totalZeros), ". Ones: " + str(totalZeros) + ". Fraction: " + str(totalOnes/totalZeros))
+        print(fileIndex)
+    print("Zeros: " + str(totalZeros), ". Ones: " + str(totalZeros) + ". Fraction: " + str(totalOnes / totalZeros))g
+
 
 path = Path("/home/jono/ML/TrainData/OrganoID_augment_2021_06_15_15_24_28/segmentations/")
 ComputeClassImbalance(path)
